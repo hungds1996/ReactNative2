9545b4cc70504a43b0084d15097ac4c32e55562e
HungDS <hungds1996@gmail.com>
2018-05-04 17:38:05 +0700

    added

--- js-practices-handout-part2/handout/Practice5/generatePractice.js
+++ js-practices-handout-part2/handout/Practice5/generatePractice.js
@@ -1,13 +1,33 @@
 'use strict'
 
-function generate(testLengthArray){
-  return Array.from({length : testLengthArray.length})
-    .map(item => ({
-      input: Array.from({length: item}).map(item => []),
-      target: 0,
-      output: -1
-    })
-  ); // Remove this line and change to your own algorithm
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
 }
+
+function vomitArray(arrIdx, count) {
+  const item = [];
+
+  function makeTargetIndex(arrIdx, arrLength) {
+    const remainder = arrIdx % 4;
+    return remainder == 0 ? 0 : 
+           remainder == 1 ? -1 :
+           remainder == 2 ? arrLength - 1 : randomInt(arrLength);
+  }
+
+  const bornArr = Array.from({ length: count }).map((u, i) => Math.floor((-10000 + (i+1) * Math.floor(20000/count) - (i+1) * Math.random() * Math.floor(20000/count))));
+  const bornTarget = makeTargetIndex(arrIdx, count);
+
+  return {
+    input : bornArr,
+    target : bornTarget == -1 ? 10001 : bornArr[bornTarget],
+    output: bornTarget
+  }
+};
+
+function generate(testLengthArray) {
+  const result = testLengthArray.map((length, index) =>vomitArray(index, length))
+  return result;
+};
+
 
 module.exports = generate
--- js-practices-handout/handout/Practice3/generatePractice.js
+++ js-practices-handout/handout/Practice3/generatePractice.js
@@ -14,36 +14,32 @@ function randomInt(range) {
   return Math.floor(Math.random() * range);
 }
 
-function vomitArray(count) {
+function vomitArray(arrIdx, count) {
   const item = [];
 
-  function born(array, first, step) {
-    array.push(first + Math.floor(Math.random() * step));
-    return array.length === count ? array : born(array, first + step, step);
+  function makeTargetIndex(arrIdx, arrLength) {
+    const remainder = arrIdx % 4;
+    return remainder == 0 ? 0 : 
+           remainder == 1 ? -1 :
+           remainder == 2 ? arrLength - 1 : randomInt(arrLength);
   }
-  
-  return born(item, -10000, Math.floor(20000/count));
+
+  const bornArr = Array.from({ length: count }).map((u, i) => Math.floor((-10000 + (i+1) * Math.floor(20000/count) - (i+1) * Math.random() * Math.floor(20000/count))));
+  const bornTarget = makeTargetIndex(arrIdx, count);
+
+  return {
+    input : bornArr,
+    target : bornTarget == -1 ? 10001 : bornArr[bornTarget],
+    output: bornTarget
+  }
 };
 
-function makeTargetIndex(arrIdx, arrLength) {
-  const remainder = arrIdx % 4;
-  return remainder == 0 ? 0 : 
-         remainder == 1 ? arrLength :
-         remainder == 2 ? arrLength + 1 : randomInt(arrLength);
-}
-
 function generate(testLengthArray) {
-  const result = testLengthArray.map((length, index) =>({
-      input: vomitArray(length),
-      target: vomitArray(length)[makeTargetIndex(index, length)],
-      output: makeTargetIndex(index, length)
-
-    })  
-  );
+  const result = testLengthArray.map((length, index) =>vomitArray(index, length))
   return result;
 };
 
-console.log(generate([1,3,4,5,6]))
+console.log(generate([1,5,9,11]))
 // console.log(vomitArray(100))
 
 // module.exports = generate
